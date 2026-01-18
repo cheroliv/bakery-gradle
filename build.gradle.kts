8812ed099f3618f41adba3f4d5d42c7bb2692f1d
cheroliv <cheroliv@laposte.net>
2026-01-18 21:51:46 +0100

    🔧 build(gradle): Remove unused wrapper configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
-import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.BIN
+//import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.BIN
 
-plugins {
-//    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.bakery)
-}
+plugins { alias(libs.plugins.bakery) }
 
 bakery { configPath = file("site.yml").absolutePath }
 
-tasks.named<Wrapper>("wrapper") {
-    gradleVersion = libs.gradle.tooling.api.get().version
-    distributionType = BIN
-}
+//tasks.named<Wrapper>("wrapper") {
+//    gradleVersion = libs.gradle.tooling.api.get().version
+//    distributionType = BIN
+//}
