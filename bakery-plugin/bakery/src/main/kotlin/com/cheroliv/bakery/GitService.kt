1dd60f6a5ecfa1cc34bb480fea5c9001aa780bf4
cheroliv <cheroliv@laposte.net>
2026-01-29 23:08:28 +0100

    Add GIT_ATTRIBUTES_CONTENT constant to GitService

--- bakery-plugin/bakery/src/main/kotlin/com/cheroliv/bakery/GitService.kt
+++ bakery-plugin/bakery/src/main/kotlin/com/cheroliv/bakery/GitService.kt
@@ -15,6 +15,20 @@ import org.slf4j.Logger
 import java.io.File
 
 object GitService {
+    const val GIT_ATTRIBUTES_CONTENT = """
+                                    #
+                                    # https://help.github.com/articles/dealing-with-line-endings/
+                                    #
+                                    # Linux start script should use lf
+                                    /gradlew        text eol=lf
+
+                                    # These are Windows script files and should use crlf
+                                    *.bat           text eol=crlf
+
+                                    # Binary files should be left untouched
+                                    *.jar           binary
+                                """
+
     sealed class FileOperationResult {
         sealed class GitOperationResult {
             data class Success(
